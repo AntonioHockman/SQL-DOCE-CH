78e1168c7f47d93cdf1e1c2c4ab78b9312740268
AntonioHockman <150075660+AntonioHockman@users.noreply.github.com>
2024-06-05 18:01:40 -0500

    updated hw

--- MAIN/server.js
+++ MAIN/server.js
@@ -57,8 +57,8 @@ async function viewDepartments() {
 }
 
 function viewAllEmployees() {
-   const sql = `SELECT employee.id, employee.first_name AS "first name", employee.last_name AS "last name" FROM employee `;
-    //  role.title, department.name AS department, role.salary, manager.first_name || ' ' || manager.last_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id`
+   const sql = `SELECT employee.id, employee.first_name AS "first name", employee.last_name AS "last name" FROM employee 
+     role.title, department.name AS department, role.salary, manager.first_name || ' ' || manager.last_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id`;
   pool.query(sql, (err, { rows }) => {
     if (err) console.log(err);
     console.table(rows);
@@ -82,37 +82,75 @@ async function addDepartment() {
                 name: 'add_department',
                 message: 'What department are you adding?'
             }
+          
         ]);
         const newDepartment = answer.add_department;
-        // console.log(`New department added: ${newDepartment}`);
-
+        await pool.query("insert into department(name) values ($1)",[newDepartment])
+        console.log(`New department added: ${newDepartment}`);
+        menu()
 }
 
 async function addRole() {
+  const departments = await pool.query("select id as value,name as name from department")
     const answer = await inquirer
        .prompt([
            {
                type:'input',
                name: 'add_role',
                message: 'What role are you adding?'
-           }
+           },
+           {
+            type:'input',
+            name: 'salary',
+            message: 'What is the salary of the role?'
+        },
+        {
+          type:'list',
+          name: 'dept_id',
+          message: 'What department does the role belong to?',
+      
+          choices: departments.rows
+        }
        ]);
-       const newDepartment = answer.add_role;
-
+        await pool.query("insert into role (title, salary,department_id) values($1,$2,$3)",[answer.add_role,answer.salary,answer.dept_id])
+        console.log("role was susccesfully added!")
+        menu();
 }
 
 async function addEmployee() {
+    const role = await pool.query('select id as value,title as name from role')
+    const managers = await pool.query( "select id as value, first_name||' ' || last_name from employee")
     const answer = await inquirer
        .prompt([
            {
                type:'input',
-               name: 'add_employee',
-               message: 'What is the first and last name of employee?'
-           }
+               name: 'first_name',
+               message: 'What is the first of employee?'
+           },
+           {
+            type:'input',
+            name: 'last_name',
+            message: 'What is the last name of employee?'
+        },
+        {
+          type:'list',
+          name: 'role_id ',
+          message: 'What is the employees role?',
+          choices: role.rows
+      },
+      {
+        type:'list',
+        name: 'manaager_id ',
+        message: 'What is the employees manager',
+        choices: managers.rows
+    }
        ]);
-       const newDepartment = answer.add_employee;
+        await pool.query("insert into employee(first_name,last_name,role_id,manager_id",[answer.first_name,answer.last_name,answer.role_id,answer.manager_id])
+        console.log('employee was sucessfully added!')
+        menu()
 
 }
+
 
 
 
--- README.md
+++ README.md
@@ -1,22 +1,68 @@
-# SQL-DOCE-CH
 
-# Motivation :
-    I was intereted in expanding my coding knowledge
+# <Your-Project-Title>
 
-# Build status:
-    I did not finish the whole project, a few requirements where not met, the majority of the inquirer prompts are functional. up to addEmployee option. 
-# contributers:
-    My tutor Megam helped me write some of the join methods. 
-# How to use :
-Run node server.js in MAIN folder, answer prompts in terminal
-# License:
- MIT 
-# Video link: 
-https://drive.google.com/file/d/16wb9WceDXh3IQXGfUcf7SoqfP5yIjYDF/view?usp=sharing
+## Description
 
-<iframe src="https://drive.google.com/file/d/16wb9WceDXh3IQXGfUcf7SoqfP5yIjYDF/preview" width="640" height="480"></iframe>
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
 
-https://youtu.be/D9RFRlRqjWA?si=fBmUrV9jO5N9jMxv
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
 
-# github repo link
-https://github.com/AntonioHockman/SQL-DOCE-CH
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
